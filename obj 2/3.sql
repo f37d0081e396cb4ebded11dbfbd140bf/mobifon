8056e84f5a7e16c20e1506e637af48d4e3470b78
f37d0081e396cb4ebded11dbfbd140bf <78140064+f37d0081e396cb4ebded11dbfbd140bf@users.noreply.github.com>
2021-01-28 15:44:38 +0700

    Update 3.sql

--- obj 2/3.sql
+++ obj 2/3.sql
@@ -1,5 +1,5 @@
 select
-	users.name as username,
+    users.name as username,
     coms.name as community_name,
     ps.name as permission_name
 from users
@@ -10,5 +10,5 @@ join community_member_permissions as cmps on cmps.member_id = cms.user_id
 join permissions as ps on ps.id = cmps.permission_id
 
 where
-	lower(users.name) like '%t%'
+    lower(users.name) like '%t%'
     or lower(ps.name) like '%articles%'
